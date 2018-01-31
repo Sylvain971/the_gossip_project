2ed6666e73fa865c6789a2efe78b3057967611ec
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-01-31 13:27:32 +0100

    ajout du controller (vide)

---
+++ db/migrate/20180131120920_create_gossips.rb
@@ -0,0 +1,10 @@
+class CreateGossips < ActiveRecord::Migration[5.1]
+  def change
+    create_table :gossips do |t|
+      t.string :anonymous_autor
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
